578678de39d17160e8fb297035f6d564612992ca
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2024-06-23 11:54:02 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -43,7 +43,7 @@ var corsOptions = {
 }
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
